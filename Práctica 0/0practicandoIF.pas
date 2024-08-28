7842ca0044234a97961c7fe096a6cd6214d276fe
Franco-Bayugar <126421782+Franco-Bayugar@users.noreply.github.com>
2024-08-28 16:27:38 -0300

    Add files via upload

---
+++ Práctica 0/0practicandoIF.pas
@@ -0,0 +1,11 @@
+program PracticandoIF;
+
+var 
+	x: integer;
+begin
+	x:= 0;
+	if(x > 0) then
+		writeln('x es mayor a 0, x es el número: ', x)
+	else
+		writeln('x no es mayor a 0, x es el número', x);
+end.
